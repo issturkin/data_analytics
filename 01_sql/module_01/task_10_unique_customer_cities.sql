c2e74ae54d7c75a320ef58d0e94cd2afd2fd016b
Sergey Turkin <issturkin@gmail.com>
2026-02-26 10:42:22 +0300

    feat(sql): add distinct customer cities task
    Добавлена задача на DISTINCT (уникальные города клиентов)

---
+++ 01_sql/module_01/task_10_unique_customer_cities.sql
@@ -0,0 +1,16 @@
+/*
+Курс: Karpov.Courses — Аналитика данных
+https://karpov.courses/analytics
+
+Задача:
+Вывести список уникальных городов,
+в которых зарегистрированы пользователи.
+
+Поле результата:
+customer_city
+---------------------------------------------------------
+*/
+
+SELECT DISTINCT
+    customer_city
+FROM customers;
