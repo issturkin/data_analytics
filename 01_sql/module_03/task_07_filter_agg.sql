3bfb2e7cb3e370d069a1e76a88757cfa09e514a8
Sergey Turkin <issturkin@gmail.com>
2026-03-03 11:41:03 +0300

    Добавлено решение задачи по агрегатным функциям с фильтром

---
+++ 01_sql/module_03/task_07_filter_agg.sql
@@ -0,0 +1,20 @@
+/*
+Курс: Karpov.Courses — Аналитика данных
+https://karpov.courses/analytics
+
+Тема: Агреггация с FILTER.
+Задача: Получить количество заказов и количество опоздавших заказов по каждому статусу заказа.
+Опоздавшим считается заказ, который был доставлен клиенту позже, чем планировалось.
+*/
+
+SELECT
+  order_status,
+  COUNT(order_id) AS total_orders,
+  COUNT(order_delivered_customer_time) FILTER(
+    WHERE
+      order_delivered_customer_time > order_estimated_delivery_time
+  ) AS late_orders
+FROM
+  orders
+GROUP BY
+  1
